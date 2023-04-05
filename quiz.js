ddcd39db584cf714c3a0aa55ade2ae81138738fd
dracula404 <127628853+dracula404@users.noreply.github.com>
2023-04-06 00:10:09 +0200

    Quiz.js has been added
    This is so cool I love life

--- quiz.js
+++ quiz.js
@@ -1,108 +1,32 @@
-var myQuestions = [
-	{
-		question: "What is 10/2?",
-		answers: {
-			a: '3',
-			b: '5',
-			c: '115'
-		},
-		correctAnswer: 'b'
-	},
-	{
-		question: "What is 30/3?",
-		answers: {
-			a: '3',
-			b: '5',
-			c: '10'
-		},
-		correctAnswer: 'c'
-	}
-];
+// Get the form and quiz results elements
+const quizForm = document.getElementById('quizForm');
+const quizResults = document.getElementById('quizResults');
 
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
+// Define the correct answers
+const correctAnswers = {
+    q1: 'a',
+    q2: 'b',
+    q3: 'c'
+};
 
-generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+// Add an event listener to the form submit button
+quizForm.addEventListener('submit', function (event) {
+    event.preventDefault(); // prevent the form from submitting
 
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+    let score = 0; // initialize the score to 0
 
-	function showQuestions(questions, quizContainer){
-		// we'll need a place to store the output and the answer choices
-		var output = [];
-		var answers;
+    // loop through all the form elements and check the answers
+    for (let i = 0; i < quizForm.elements.length; i++) {
+        const element = quizForm.elements[i];
+        if (element.type === 'radio' && element.checked) {
+            const answer = element.value;
+            const questionNumber = element.name;
+            if (answer === correctAnswers[questionNumber]) {
+                score++; // increment the score for correct answers
+            }
+        }
+    }
 
-		// for each question...
-		for(var i=0; i<questions.length; i++){
-			
-			// first reset the list of answers
-			answers = [];
-
-			// for each available answer...
-			for(letter in questions[i].answers){
-
-				// ...add an html radio button
-				answers.push(
-					'<label>'
-						+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
-						+ letter + ': '
-						+ questions[i].answers[letter]
-					+ '</label>'
-				);
-			}
-
-			// add this question and its answers to the output
-			output.push(
-				'<div class="question">' + questions[i].question + '</div>'
-				+ '<div class="answers">' + answers.join('') + '</div>'
-			);
-		}
-
-		// finally combine our output list into one string of html and put it on the page
-		quizContainer.innerHTML = output.join('');
-	}
-
-
-	function showResults(questions, quizContainer, resultsContainer){
-		
-		// gather answer containers from our quiz
-		var answerContainers = quizContainer.querySelectorAll('.answers');
-		
-		// keep track of user's answers
-		var userAnswer = '';
-		var numCorrect = 0;
-		
-		// for each question...
-		for(var i=0; i<questions.length; i++){
-
-			// find selected answer
-			userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
-			
-			// if answer is correct
-			if(userAnswer===questions[i].correctAnswer){
-				// add to the number of correct answers
-				numCorrect++;
-				
-				// color the answers green
-				answerContainers[i].style.color = 'lightgreen';
-			}
-			// if answer is wrong or blank
-			else{
-				// color the answers red
-				answerContainers[i].style.color = 'red';
-			}
-		}
-
-		// show number of correct answers out of total
-		resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
-	}
-
-	// show questions right away
-	showQuestions(questions, quizContainer);
-	
-	// on submit, show results
-	submitButton.onclick = function(){
-		showResults(questions, quizContainer, resultsContainer);
-	}
-
-}
+    // display the quiz results
+    quizResults.innerHTML = `Összesen ${score} pontot kaptál a ${Object.keys(correctAnswers).length}-ból.`;
+});
